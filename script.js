aabf98771df40c4fc8293b28eb890675a8d84bf3
Pavibharathi-16112004 <b.pavi2022@gmail.com>
2026-07-10 14:28:39 +0530

    update API URL production backend

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE =  "http://127.0.0.1:8000";
 
 const messageBox = document.getElementById("message");
 const loginForm = document.getElementById("loginForm");
