05c80999875ade32c7a65cf156bce6dc338fa9db
Pavibharathi-16112004 <b.pavi2022@gmail.com>
2026-07-10 19:13:57 +0530

    fix API URL in correct frontend script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_BASE =  "http://127.0.0.1:8000";
+const API_BASE =  "https://uni-nu-five.vercel.app";
 
 const messageBox = document.getElementById("message");
 const loginForm = document.getElementById("loginForm");
